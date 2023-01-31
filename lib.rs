2959bbf0039a31faf34b8dbe553bf4f4ccc11dea
Asadullah Khan <aukhan@akru.co>
2023-01-31 12:26:37 +0500

    add mapping

--- lib.rs
+++ lib.rs
@@ -4,21 +4,34 @@ use ink_lang as ink;
 
 #[ink::contract]
 mod flipper {
+    use ink_storage::traits::SpreadAllocate;
 
     /// Defines the storage of your contract.
     /// Add new fields to the below struct in order
     /// to add new static storage fields to your contract.
     #[ink(storage)]
+    #[derive(SpreadAllocate)]
     pub struct Flipper {
         /// Stores a single `bool` value on the storage.
         value: bool,
+        caller: AccountId,
+        caller_to_number: ink_storage::Mapping<AccountId, u32>,
+
     }
 
     impl Flipper {
         /// Constructor that initializes the `bool` value to the given `init_value`.
         #[ink(constructor)]
         pub fn new(init_value: bool) -> Self {
-            Self { value: init_value }
+            // This call is required in order to correctly initialize the
+            // `Mapping`s of our contract.
+            ink_lang::utils::initialize_contract(|contract: &mut Self| {
+                let caller:AccountId = Self::env().caller();
+                let num:u32 = 0;
+                contract.value = init_value;
+                contract.caller = caller;
+                contract.caller_to_number.insert(&caller, &(&num+1));
+            })
         }
 
         /// Constructor that initializes the `bool` value to `false`.
@@ -26,7 +39,7 @@ mod flipper {
         /// Constructors can delegate to other constructors.
         #[ink(constructor)]
         pub fn default() -> Self {
-            Self::new(Default::default())
+            ink_lang::utils::initialize_contract(|_| {})
         }
 
         /// A message that can be called on instantiated contracts.
@@ -34,13 +47,31 @@ mod flipper {
         /// to `false` and vice versa.
         #[ink(message)]
         pub fn flip(&mut self) {
+            
+            let caller:AccountId = Self::env().caller();
+            let num:u32 = self.caller_to_number.get(caller).unwrap_or_default();
+
+            self.caller = caller;
             self.value = !self.value;
+            self.caller_to_number.insert(caller, &(&num+1));
+
         }
 
         /// Simply returns the current value of our `bool`.
         #[ink(message)]
         pub fn get(&self) -> bool {
             self.value
+        }
+        /// Simply returns the current value of our `AccountId`.
+        #[ink(message)]
+        pub fn get_caller(&self) -> AccountId {
+            self.caller
+        }
+
+        /// Simply returns the current value of our `u32`.
+        #[ink(message)]
+        pub fn get_caller_value(&self,caller_id:AccountId) -> u32 {
+            self.caller_to_number.get(caller_id).unwrap_or_default()
         }
     }
 
